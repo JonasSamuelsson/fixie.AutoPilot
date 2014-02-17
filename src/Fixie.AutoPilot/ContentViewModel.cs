e53b1844fd1b23af60dc74ce10a4215425f0b75a
Jonas Samuelsson <jonas@samuelsson.at>
2014-02-17 09:11:47 +0100

    compiles and tests selected solution

--- src/Fixie.AutoPilot/ContentViewModel.cs
+++ src/Fixie.AutoPilot/ContentViewModel.cs
@@ -36,63 +36,52 @@ namespace Fixie.AutoPilot
             _cancellationTokenSource = new CancellationTokenSource();
 
             Task.Factory.StartNew(async () =>
-            {
-                var msbuildProcess = Process.Start(new ProcessStartInfo(msbuild)
-                {
-                    Arguments = @event.Solution + msbuildArgs,
-                    CreateNoWindow = true,
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false
-                });
-                Text.Value = msbuildProcess.StandardOutput.ReadToEnd();
-                msbuildProcess.WaitForExit();
-                if (msbuildProcess.ExitCode == 0)
-                {
-                    var originalWriter = Console.Out;
-                    try
-                    {
-                        using (var writer = new StringWriter())
-                        {
-                            Console.SetOut(writer);
-                            var appDomain = AppDomain.CreateDomain(Guid.NewGuid().ToString());
-                            var launcher =
-                                (Launcher)
-                                    appDomain.CreateInstanceAndUnwrap(typeof(Launcher).Assembly.FullName,
-                                        typeof(Launcher).FullName);
+                                        {
+                                            while (true)
+                                            {
+                                                var msbuildProcess = Process.Start(new ProcessStartInfo(msbuild)
+                                                                                   {
+                                                                                       Arguments = @event.Solution + msbuildArgs,
+                                                                                       CreateNoWindow = true,
+                                                                                       RedirectStandardOutput = true,
+                                                                                       UseShellExecute = false
+                                                                                   });
+                                                Text.Value += msbuildProcess.StandardOutput.ReadToEnd();
+                                                msbuildProcess.WaitForExit();
+                                                if (msbuildProcess.ExitCode == 0)
+                                                {
+                                                    //try
+                                                    //{
+                                                    //    var appDomain = AppDomain.CreateDomain(Guid.NewGuid().ToString());
+                                                    //    var launcher = (Launcher)appDomain.CreateInstanceAndUnwrap(typeof(Launcher).Assembly.FullName, typeof(Launcher).FullName);
 
-                            var dir = Path.GetDirectoryName(GetType().Assembly.Location);
-                            var fixie = Path.Combine(dir, "Fixie.Console.exe");
-                            launcher.Execute(fixie, GetPaths(@event.Solution));
-                            Text.Value += writer.ToString();
-                        }
-                    }
-                    catch (Exception exception)
-                    {
-                        Text.Value += exception.ToString();
-                    }
-                    finally
-                    {
-                        Console.SetOut(originalWriter);
-                    }
+                                                    //    var dir = Path.GetDirectoryName(GetType().Assembly.Location);
+                                                    //    var fixie = Path.Combine(dir, "Fixie.Console.exe");
+                                                    //    Text.Value += launcher.Execute(fixie, GetPaths(@event.Solution));
+                                                    //}
+                                                    //catch (Exception exception)
+                                                    //{
+                                                    //    Text.Value += exception.ToString();
+                                                    //}
 
-                    //var dir = Path.GetDirectoryName(GetType().Assembly.Location);
-                    //var fixie = Path.Combine(dir, "Fixie.Console.exe");
+                                                    var dir = Path.GetDirectoryName(GetType().Assembly.Location);
+                                                    var fixie = Path.Combine(dir, "Fixie.Console.exe");
 
-                    //AppDomain.CreateDomain("", null, new AppDomainSetup{})
-                    //var fixieProcess = Process.Start(new ProcessStartInfo(fixie)
-                    //{
-                    //    Arguments = GetPaths(@event.Solution),
-                    //    CreateNoWindow = true,
-                    //    RedirectStandardOutput = true,
-                    //    UseShellExecute = false,
-                    //    WorkingDirectory = dir
-                    //});
-                    //Text.Value += fixieProcess.StandardOutput.ReadToEnd();
-                    //fixieProcess.WaitForExit();
-                }
+                                                    var fixieProcess = Process.Start(new ProcessStartInfo(fixie)
+                                                                                     {
+                                                                                         Arguments = string.Join(" ", GetPaths(@event.Solution)),
+                                                                                         CreateNoWindow = true,
+                                                                                         RedirectStandardOutput = true,
+                                                                                         UseShellExecute = false,
+                                                                                         WorkingDirectory = dir
+                                                                                     });
+                                                    Text.Value += fixieProcess.StandardOutput.ReadToEnd();
+                                                    fixieProcess.WaitForExit();
+                                                }
 
-                await Task.Delay(TimeSpan.FromSeconds(10));
-            },
+                                                await Task.Delay(TimeSpan.FromSeconds(3));
+                                            }
+                                        },
                 _cancellationTokenSource.Token,
                 TaskCreationOptions.LongRunning,
                 TaskScheduler.Current);
@@ -106,14 +95,20 @@ namespace Fixie.AutoPilot
                 .Where(x => x.StartsWith("Project("))
                 .Select(x => x.Split(separators, StringSplitOptions.None)[1].Trim('"'))
                 .Select(x => Path.Combine(dir, x, @"bin\debug", x + ".dll"))
+                .Where(x => File.Exists(Path.Combine(Path.GetDirectoryName(x), "fixie.dll")))
                 .ToArray();
         }
 
         private class Launcher : MarshalByRefObject
         {
-            public void Execute(string exe, string[] args)
+            public string Execute(string exe, string[] args)
             {
-                Assembly.LoadFile(exe).EntryPoint.Invoke(null, new object[] { args });
+                using (var writer = new StringWriter())
+                {
+                    Console.SetOut(writer);
+                    Assembly.LoadFile(exe).EntryPoint.Invoke(null, new object[] { args });
+                    return writer.ToString();
+                }
             }
         }
     }
