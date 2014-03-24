f206f80e453cee65e69a4a0cea6d176436680ff7
Jonas Samuelsson <jonas@samuelsson.at>
2014-03-24 22:42:57 +0100

    added test project

---
+++ src/Fixie.AutoRun.Tests/Converters/InvertedBoolConverterTests.cs
@@ -0,0 +1,18 @@
+﻿using Fixie.AutoRun.Converters;
+using Shouldly;
+
+namespace Fixie.AutoRun.Tests.Converters
+{
+   public class InvertedBoolConverterTests
+   {
+      public void ShouldConvertFalseToTrue()
+      {
+         InvertedBoolConverter.Instance.Convert(false, null, null, null).ShouldBe(true);
+      }
+
+      public void ShouldConvertTrueToFalse()
+      {
+         InvertedBoolConverter.Instance.Convert(false, null, null, null).ShouldBe(true);
+      }
+   }
+}
---
+++ src/Fixie.AutoRun.Tests/CustomConvention.cs
@@ -0,0 +1,15 @@
+﻿using Fixie.Conventions;
+using System.Linq;
+
+namespace Fixie.AutoRun.Tests
+{
+   class CustomConvention : Convention
+   {
+      public CustomConvention()
+      {
+         var suffixes = new[] { "Test", "Tests" };
+         Classes.Where(x => suffixes.Any(y => x.Name.EndsWith(y)));
+         Methods.Where(x => x.IsPublic && x.IsVoid());
+      }
+   }
+}
